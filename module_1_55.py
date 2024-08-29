a81ca5a52195050d0cc22b5a9c89b858cf7dc81f
Sanny2306 <rastegaev.90@bk.ru>
2024-08-29 20:22:01 +0300

    Initial commit

---
+++ module_1_55.py
@@ -0,0 +1,8 @@
+immutable_var = 1 , 2 , "Sanny", "Hommework"
+print(immutable_var)
+#immutable_var [0] = 0
+#print(immutable_var)
+immutable_list = ([1, 2], 3,)
+print(immutable_list)
+immutable_list [0][0]= 2
+print(immutable_list)
