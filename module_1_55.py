db6a079ba6c6a2fb5a575cd1e39ab0dd44540ae2
Sanny2306 <rastegaev.90@bk.ru>
2024-09-03 17:19:31 +0300

    исправление

--- module_1_55.py
+++ module_1_55.py
@@ -2,7 +2,7 @@ immutable_var = 1 , 2 , "Sanny", "Hommework"
 print(immutable_var)
 #immutable_var [0] = 0
 #print(immutable_var)
-immutable_list = ([1, 2], 3,)
-print(immutable_list)
-immutable_list [0][0]= 2
-print(immutable_list)
+mutable_list = [0, 1, 2, 3]
+print(mutable_list[0])
+mutable_list [0]= 8
+print(mutable_list)
